30843d302c8363ce2be99425646c348204ccf8e4
Kizmet <bryantpatton@gmail.com>
2020-01-15 10:44:38 -0800

    resize

--- src/hashtable.py
+++ src/hashtable.py
@@ -50,42 +50,74 @@ class HashTable:
 
         Fill this in.
         """
-        # find the index
+        # generate the index
         i = self._hash_mod(key)
-        # if bucket is in use
-        while self.storage[i] != None:
-            # creat new LL
-            entry = LinkedPair(key, value)
-            bucket = self.storage[i]
-            # if just updating value, set value and return
-            if bucket.key == key:
-                bucket.value = value
-                return
-            # loop through bucket  and instert new
-            while bucket.next != None:
-                bucket = bucket.next
-                if bucket.key == key:
-                    bucket.value = value
-                    return
+        # set var for current pair
+        current_pair = self.storage[i]
+        # set var for last pair
+        last_pair = None
+        # loop through pair until find the key
+        while current_pair is not None and current_pair.key != key:
+            # set the last pair
+            last_pair = current_pair
+            # point to next pair
+            current_pair = last_pair.next
+        # if key exists update the value
+        if current_pair is not None:
+            current_pair.value = value
+        # create the new pair
+        else:
+            new_pair = LinkedPair(key, value)
+            # point to current
+            new_pair.next = self.storage[i]
+            # set new pair at index
+            self.storage[i] = new_pair
 
-            # set the next pair to the new_pair
-            bucket.next = entry
-            self.items += 1
-
-        # if index is none
-        self.storage[i] = LinkedPair(key, value)
         self.items += 1
-        # storage[i] = value
 
     def remove(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         """
-        pass
+        # call self._hash_mod(key) and get the index of the insertion
+        index = self._hash_mod(key)
+
+        # check to see if the value at the index is not None
+        if self.storage[index] is not None:
+            # set the current pair to the head of the linked list
+            current_pair = self.storage[index]
+
+            # if current pair key == key or there is no next pair, set the current index to current_pair next
+            if current_pair.key == key or current_pair.next is None:
+                self.storage[index] = current_pair.next
+                # decrement the count
+                self.items -= 1
+            # otherwise
+            else:
+                # loop through the linked list
+                while current_pair is not None:
+                    # let next pair = current pair next
+                    next_pair = current_pair.next
+
+                    # if next pair key == key
+                    if next_pair.key == key:
+                        # set current pair next to next pair next
+                        current_pair.next = next_pair.next
+                        # decrement the count
+                        self.items -= 1
+
+                    # set current pair to current pair next
+                    current_pair = current_pair.next
+        # otherwise, print a warning
+        else:
+            print("Error, key is not there")
+
+        # check to see if the load factor is under 0.2
+        if (self.items / len(self.storage)) < 0.2:
+            # call self.shrink to halve the size of the hash table's storage
+            self.shrink()
 
     def retrieve(self, key):
         """
@@ -114,13 +146,53 @@ class HashTable:
             return None
 
     def resize(self):
-        """
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
+        # double the capacity
+        self.capacity *= 2
+        # reset the item count to zero
+        self.items = 0
+        # store old storage to put into new storage
+        old_storage = self.storage
+        # make the new storage
+        self.storage = [None] * self.capacity
+        current_pair = None
+        # loop through each item in old storage
+        for i in range(len(old_storage)):
+            # set temp variable
+            current_pair = old_storage[i]
+            # loop through linked list
+            while current_pair is not None:
+                # insert element
+                self.insert(current_pair.key, current_pair.value)
+                current_pair = current_pair.next
 
+    def shrink(self):
+        """
+        Halves the capacity of the hash table and
+        rehash all key/value pairs.
         Fill this in.
         """
-        pass
+        # halve the size of self.capacity
+        self.capacity = int(self.capacity / 2)
+        # reset the item count to zero
+        self.items = 0
+        # let old_storage hold self.storage
+        old_storage = self.storage
+        # point self.storage to a list of capacity self.capacity
+        self.storage = [None] * self.capacity
+
+        # loop through old_storage and rehash every key/value pair
+        for item in old_storage:
+            # if item is not None
+            if item is not None:
+                # set the head of the linked list to current_pair
+                current_pair = item
+
+                # loop through the linked list
+                while current_pair is not None:
+                    # call self.insert with the key/value of the linked pair
+                    self.insert(current_pair.key, current_pair.value)
+                    # set current_pair to current_pair next
+                    current_pair = current_pair.next
 
 
 if __name__ == "__main__":
